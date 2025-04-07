d5bdb0e19952886c5b93a1b4dabef77581bf4a35
Nagendra Kumar <149855963+nagendrakr16@users.noreply.github.com>
2025-04-07 22:03:05 +0530

    Delete Backend directory

--- Backend/models/Document.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require('mongoose');
-
-const DocumentSchema = new mongoose.Schema({
-  title: { type: String, required: true },
-  content: { type: String, default: '' }
-}, { timestamps: true });
-
-module.exports = mongoose.model('Document', DocumentSchema);
